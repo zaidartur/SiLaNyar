e74ea0cb6f824ee9a847dc72b2513ccb62d66946
wyasana12 <140244781+wyasana12@users.noreply.github.com>
2025-04-19 20:34:00 +0700

    Update Fitur V1.1

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use App\Models\customer;
+use App\Models\Customer;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
