0058737c5615ec1aa4f3053f1bfcdc57a41fa038
wyasana12 <140244781+wyasana12@users.noreply.github.com>
2025-04-19 20:52:15 +0700

    Update Fitur V1.1

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -35,4 +35,11 @@ class CustomerFactory extends Factory
             'remember_token' => Str::random(10),
         ];
     }
+
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
 }
